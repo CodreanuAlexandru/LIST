d126d80dfa8e177d900eee1a420788e62fd24cfe
CodreanuAlexandru <70651712+CodreanuAlexandru@users.noreply.github.com>
2021-01-19 22:13:37 +0200

    Add files via upload

---
+++ Problema.2.py
@@ -0,0 +1,14 @@
+with open('input.txt','r')as f:
+    lista1=list(eval(f.read()))
+print(lista1)
+lista2=sorted(lista1)
+print(lista2)
+lista3=sorted(lista1,reverse=True)
+print(lista3)
+print(len(lista1))
+print(max(lista1),min(lista1),sep="\n")
+lista4=lista1+[111]
+print(lista4)
+lista5=lista1
+lista5.insert(1, 222)
+print(lista5)
